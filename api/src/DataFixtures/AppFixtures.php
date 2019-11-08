20715c1c61af6f2c4bcef4b583d85367be34e373
Guillaume NEDELEC <guillaume.nedelec3308@gmail.com>
2019-11-08 03:01:39 +0100

    jwt auth on symfony + add prefix "/api" on every api routes + user fixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,17 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Controller\UserController;
 use App\Entity\Project;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
         for($i=0; $i< 10; $i++ ) {
             $project = new Project();
             $project->setName("project_" . $i);
@@ -21,6 +28,14 @@ class AppFixtures extends Fixture
             $manager->persist($project);
         }
 
+        $user = new User();
+        $user->setEmail("johndoe@example.com");
+        $user->setName("John Doe");
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'test'
+        ));
+        $manager->persist($user);
         $manager->flush();
     }
 }
--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -113,5 +114,25 @@ class User
         }
 
         return $this;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials()
+    {
+        return null;
     }
 }
