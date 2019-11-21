1d163041bd922879bea83a995649486a6cc86371
Guillaume NEDELEC <guillaume.ned33@gmail.com>
2019-11-22 00:17:18 +0100

    front update

--- front/pages/releases.php
+++ front/pages/releases.php
@@ -4,6 +4,13 @@
             <div class="col-sm-6">
                 <h1>Releases</h1>
             </div>
+            <div class="col-sm-6">
+                <div class="float-sm-right">
+                    <button type="button" id="add-btn" class="btn btn-primary" data-toggle="modal" data-target="#modal-release">
+                        Ajouter une release
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </section>
@@ -21,6 +28,7 @@
                             <th>Date</th>
                             <th>Description</th>
                             <th>Sources</th>
+                            <th>Actions</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -31,6 +39,48 @@
         </div>
     </div>
 </section>
+
+<!-- Modal -->
+<div class="modal fade" id="modal-release" tabindex="-1" role="dialog" aria-labelledby="add-release-modal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Nouvelle release</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <input type="hidden" class="form-control" id="id" value="">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Titre:</label>
+                        <input type="text" class="form-control" id="name" placeholder="Titre" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="version">Version:</label>
+                        <input type="text" class="form-control" id="version" placeholder="1.0.0" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description:</label>
+                        <textarea class="form-control" rows="5" cols="60" id="description" placeholder="Description"></textarea><br>
+                    </div>
+                    <div class="form-group">
+                        <label for="src_link">Sources:</label>
+                        <input type="text" class="form-control" id="src_link" placeholder="Lien vers les sources" required>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                <button type="button" form="add-project" id="modal-mode" class="btn btn-primary" value="create">Enregistrer</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/releasesScript.js"></script>
 <script>
     $(function () {
         $('#releasesTable').DataTable({
@@ -41,11 +91,5 @@
             "info": true,
             "autoWidth": false,
         });
-        /*sendAjax('/api/releases/' + localStorage.getItem('user_project_selected'))
-            .then(res => {
-
-            }).catch(err => {
-
-        })*/
     });
 </script>
